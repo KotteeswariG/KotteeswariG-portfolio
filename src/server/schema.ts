043fb7e86c766bdb5e52c5192a4b86b7a856cb36
kotteeswari <kotteeswarieasu@gmail.com>
2026-02-05 23:02:55 +1000

    small schema fix

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -99,3 +99,4 @@ export type Subcategory = typeof subcategories.$inferSelect;
 export type NewSubcategory = typeof subcategories.$inferInsert;
 export type Article = typeof articles.$inferSelect;
 export type NewArticle = typeof articles.$inferInsert;
+// added created_at default
